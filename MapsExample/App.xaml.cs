85e16b1c1478e05ffcb0a540d65743f215497443
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-09-17 10:35:43 -0300

    feat: Added APIKey, AddedMaps for MapsClass

---
+++ MapsExample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MapsExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MapsExample/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Maps;
+
+namespace MapsExample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //Inicialização do Mapa
+            var location = new Location(36.95,-122.0);
+            var mapSpan = new MapSpan(location, 0.01,0.01);
+            map.MoveToRegion(mapSpan);
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
